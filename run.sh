4e8391f44111c1cf0b59402a7522ba4588a49ca7
Corey Shupe <corey@easy.gg>
2023-08-13 06:14:45 -0400

    Integrate deployment script

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Build the Docker image
-docker build . -t web-app
-
-# Stop and remove the previous running container if one exists
-docker stop web-app-container 2>/dev/null || true
-docker rm web-app-container 2>/dev/null || true
-
-# Start the new Docker container
-docker run --detach --name web-app-container -p 8000:3000 -t web-app
+./deploy.sh
 docker logs -f web-app-container
